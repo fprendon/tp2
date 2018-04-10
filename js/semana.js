eec3de465ec520107c0edf5b6a5403ecc7e8c87e
fprendon <fprendon@outlook.com>
2018-04-10 20:11:11 -0300

    tercer commit

--- js/semana.js
+++ js/semana.js
@@ -6,11 +6,11 @@ var finde = ["SABADO", "DOMINGO"];
 for (var i = 0; i < habil.length; i++) {
   if (dia === habil[i]) {
     console.log("El dia es habil");
-    continue;
+    break;
   }
   else if (dia === finde[i]) {
     console.log("El dia es fin de semana");
-    continue;
+    break;
   }
   else {
     console.log("No ha ingresado un dia");
