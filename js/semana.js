a857e6ff4dd9d7f8b7751c43d4d73e4a5e0fd6de
fprendon <fprendon@outlook.com>
2018-04-10 20:17:34 -0300

    4to commit

--- js/semana.js
+++ js/semana.js
@@ -1,19 +1,21 @@
 
 var dia = prompt ("Ingrese un dia de la semana");
-var dia = dia.toUpperCase();
+dia = dia.toUpperCase();
 var habil = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES"];
 var finde = ["SABADO", "DOMINGO"];
+
 for (var i = 0; i < habil.length; i++) {
   if (dia === habil[i]) {
     console.log("El dia es habil");
     break;
   }
-  else if (dia === finde[i]) {
+}
+
+for (var i = 0; i < finde.length; i++) {
+
+  if (dia === finde[i]) {
     console.log("El dia es fin de semana");
     break;
   }
-  else {
-    console.log("No ha ingresado un dia");
-    break;
-  }
+
 }
